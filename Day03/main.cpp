dfad113c01b98464a31cd5ffb082ab4c88b3ba28
Aiden <marcin.madry@interia.com>
2022-12-05 19:11:06 +0100

    Day 03

--- Day03/main.cpp
+++ Day03/main.cpp
@@ -61,13 +61,15 @@ internal Pair get_data(const char *filename)
     size_t size = ftell(file);
     fseek(file, 0, SEEK_SET);
 
-    char *buff = (char *) malloc(size);
+    char *buff = (char *) malloc(size + 1);
     fread(buff, 1, size, file);
     fclose(file);
     
+    buff[size] = '\0';
+    
     String slurped = {
         buff,
-        strlen(buff)
+        size
     };
 
     std::vector<String> lines;
